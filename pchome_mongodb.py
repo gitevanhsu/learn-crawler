6ca0273c8911264ff59e1a34a8fac04c8f706da2
gitevanhsu <aqwe78965@gmail.com>
2022-01-02 12:59:09 +0800

    save pchome crawler data to mongoDB server.

--- pchome_mongodb.py
+++ pchome_mongodb.py
@@ -5,19 +5,26 @@ import re
 from pymongo import MongoClient
 client = MongoClient()
 
-d = {'name':'Evan', 
-    'age':'27',
-    'gender':'man',
-}
 
-client.pchome.products.insert_one(d)
+for i in range(1, 101):
+    url ='https://ecshweb.pchome.com.tw/search/v3.3/all/results?q=%E6%9B%B2%E9%9D%A2%E8%9E%A2%E5%B9%95&page={}&sort=sale/dc'.format(i) 
+    r = requests.get(url)
+    if r.status_code != requests.codes.ok:
+        print(i)
+        print('error', status_code)
+        continue
 
-# for i in range(1, 4):
-#     url ='https://ecshweb.pchome.com.tw/search/v3.3/all/results?q=%E6%9B%B2%E9%9D%A2%E8%9E%A2%E5%B9%95&page={}&sort=sale/dc'.format(i) 
-#     r = requests.get(url)
-#     if r.status_code != requests.codes.ok:
-#         print(i)
-#         print('error', status_code)
-#         continue
+    else:
+        try:
+            data = r.json()
+            for product in data['prods']:
+                client.pchome.products.insert_one(product)
+
+        except:
+            print('ERROR',data)
+
+
+
+
 
     
